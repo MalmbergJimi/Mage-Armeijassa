a710d8023fb5fdf32d7643c6ef15515c3fda9c32
Jimi Malmberg <K8718@student.jamk.fi>
2017-04-06 13:19:06 +0300

    Pohja

---
+++ mage/Magehahmo.xaml.cs
@@ -0,0 +1,119 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace mage
+{
+    public sealed partial class Magehahmo : UserControl
+    {
+
+        // Animate magehahmo timer, ajastin milloin "kuvaa" vaihdetaan
+        private DispatcherTimer timer;
+
+        // Offset to show, eli mikä kohta Magen kuvasta näytetään suorakulmion sisällä
+        private int currentFrame = 0;
+        private int direction = 1; // 1 or -1 Vaihtuuko frame "isommaksi" vai "pienemmäksi"
+        private int frameheight = 400;
+
+        // Nopeus
+        private readonly double MaxSpeed = 10;      // Maxnopeus Magehahmolle
+        private readonly double Accelerate = 0.5;   // Kiihtyvyys, tarvitaanko?
+        private double speed;   // Nopeus
+
+        // Angle, magehahmon kulma
+        private readonly double AngleStep = 5;
+        private double Angle = 0;
+
+
+        // magehahmon sijainti Canvaksella
+        public double LocationX { get; set; }
+        public double LocationY { get; set; }
+
+        public Magehahmo()
+        {
+            this.InitializeComponent();
+
+            // Animate 
+            timer = new DispatcherTimer();
+            // 125ms,   Aikaväli päivittämiselle (fps)
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 125);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            // Current frame 0,1,2,3,4,5,6,7   nykyinen frame Magesta
+            if (direction == 1) currentFrame++;
+            else currentFrame--;
+            if (currentFrame == 0 || currentFrame == 7)
+            {
+                direction = -1 * direction; // 1 tai -1
+            }
+            // Set offset
+            SpriteSheetOffset.Y = currentFrame * -frameheight;
+        }
+
+        // Move -metodi liikkumiseen
+        public void MoveLeft()
+        {
+            // Lisää speediä
+            speed += Accelerate;
+            if (speed > MaxSpeed) speed = MaxSpeed;
+            // Uusi sijainti
+            LocationX = LocationX - speed;
+        }
+
+        public void MoveRight()
+        {
+            // Lisää speediä
+            speed += Accelerate;
+            if (speed > MaxSpeed) speed = MaxSpeed;
+
+            // Uusi sijainti
+            LocationX = LocationX + speed;
+        }
+
+        //public bool Jumping = false;
+
+        public void Jump()
+        {
+            //Jumping = true;
+
+            // Mage liikkuu ainoastaan ylöspäin LocationY = LocationY - speed;
+            // Magen liikutus sinikäyrällä
+            Angle = Angle + 0.1f;
+            if (Angle > 360) { Angle = 0; }
+            LocationY = 300 + Math.Cos(Angle) * 140;
+        }
+
+        // KÄÄNTYMINEN MIETI TÄMÄ UUSIKSI, ONKO TÄYSIN TURHA?
+        public void Rotate(int direction)
+        {
+            Angle += direction * AngleStep; // -1 * 5 tai 1 * 5
+            MagehahmoRotateAngle.Angle = Angle;
+        }
+
+        // Update location, päivitetään hahmon sijainti Canvaksella
+        public void SetLocation()
+        {
+            SetValue(Canvas.LeftProperty, LocationX);
+            SetValue(Canvas.TopProperty, LocationY);
+        }
+
+    }
+}
--- mage/MainPage.xaml.cs
+++ mage/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -13,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace mage
 {
@@ -22,9 +24,170 @@ namespace mage
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        // Luodaan magehahmo
+        private Magehahmo magehahmo;
+        //Luodaan Skappari
+        private Skappari skappari;
+        //Luodaan Rynkky
+        private Rynkky rynkky;
+        // Luodaan tellut
+        private List<Tellu> tellut;
+
+        // Tutkitaan mitkä näppäimet ovat painettuina tai päästettyinä
+        private bool UpPressed;
+        private bool LeftPressed;
+        private bool RightPressed;
+
+        // Luodaan ajastin timeri
+        private DispatcherTimer timer;
+
+
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            magehahmo = new Magehahmo
+            {
+                LocationX = 1000,     // Määritetään magehahmon aloitussijainti
+                LocationY = 100
+            };
+            // Luodaan skappari
+            Skappari skappari = new Skappari();
+            Canvas.SetTop(skappari, 400);
+            Canvas.SetLeft(skappari, 200);
+
+            // Luodaan Rynkky
+            Rynkky rynkky = new Rynkky();
+            Canvas.SetTop(rynkky, 400);
+            Canvas.SetLeft(rynkky, 1000);
+
+            // alustetaan tellulista
+            tellut = new List<Tellu>();
+            // Luodaan Tellu1
+            Tellu tellu1 = new Tellu();
+            Canvas.SetTop(tellu1, 200);
+            Canvas.SetLeft(tellu1, 300);
+            tellut.Add(tellu1);
+
+            // Luodaan Tellu2
+            Tellu tellu2 = new Tellu();
+            Canvas.SetTop(tellu2, 200);
+            Canvas.SetLeft(tellu2, 700);
+            tellut.Add(tellu2);
+
+            // Lisätään magehahmo Canvakselle
+            MyCanvas.Children.Add(magehahmo);
+            MyCanvas.Children.Add(skappari);
+            MyCanvas.Children.Add(rynkky);
+            MyCanvas.Children.Add(tellu1);
+            MyCanvas.Children.Add(tellu2);
+
+
+            // Key Listeners,   näppäimien kuuntelua, onko jokin painettuna vai ei?
+            Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;  // Onko näppäimi alhaalla
+            Window.Current.CoreWindow.KeyUp += CoreWindow_KeyUp; //Onko näppäimiä "ylhäällä"
+
+            // Start game loop,     PELI LÄHTEE HETI KÄYNTIIN
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 1000 / 60);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
         }
+
+
+        // KeyUp haistelu
+        private void CoreWindow_KeyUp(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            switch (args.VirtualKey)    // Tutkii tableteilta, puhelimilta, koneilta
+            {
+                case VirtualKey.Up:
+                    UpPressed = false;
+                    break;
+                case VirtualKey.Left:
+                    LeftPressed = false;
+                    break;
+                case VirtualKey.Right:
+                    RightPressed = false;
+                    break;
+
+            }
+        }
+        // KeyDown haistelu
+        private void CoreWindow_KeyDown(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            switch (args.VirtualKey)    // Tutkii tableteilta, puhelimilta, koneilta
+            {
+                case VirtualKey.Up:
+                    UpPressed = true;
+                    break;
+                case VirtualKey.Left:
+                    LeftPressed = true;
+                    break;
+                case VirtualKey.Right:
+                    RightPressed = true;
+                    break;
+            }
+        }
+
+        //Peli luuppi
+        private void Timer_Tick(object sender, object e)
+        {
+            // Move butterfly if up pressed
+            if (LeftPressed) magehahmo.MoveLeft();
+            if (RightPressed) magehahmo.MoveRight();
+            if (UpPressed) magehahmo.Jump();
+
+            /*
+            if (UpPressed && !magehahmo.Jumping) magehahmo.Jump();
+            */
+
+
+            // magehahmon paikka Canvaksella päivitetään
+            magehahmo.SetLocation();
+
+
+            // Collision...
+            CheckCollision();
+            // LISÄTÄÄN COLLISION MYÖHEMMIN
+        }
+
+        // BUTTON ETUSIVULLE SIIRTYMISEEN
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Etusivu));
+        }
+
+
+        private void CheckCollision()
+        {
+            // Loop flower list, Käydään läpi kukkalista
+            foreach (Tellu tellu in tellut)
+            {
+                // Get Rects, katsotaan osuuko mikään kukkalistan kukista perhoseen
+                Rect BRect = new Rect(                                               // magehahmon sijainti ja koko
+                    magehahmo.LocationX, magehahmo.LocationY, magehahmo.ActualWidth, magehahmo.ActualHeight
+                    );
+                Rect FRect = new Rect(                                               // Tellun sijainti ja koko
+                    tellu.LocationX, tellu.LocationY, tellu.ActualWidth, tellu.ActualHeight
+                    );
+                // Does objects intersects, törmääkö objektit
+                BRect.Intersect(FRect);
+                if (!BRect.IsEmpty) // Jos palautettu arvo EI OLE TYHJÄ
+                {
+                    // Collision! Area isn't empty, törmäys - alue ei ole tyhjä
+                    // Poistetaan tellu Canvakselta
+                    MyCanvas.Children.Remove(magehahmo);
+                    // Poistetaan myös listasta tellu
+                    tellut.Remove(tellu);
+
+
+                    break;
+                }
+
+            }
+        }
+
     }
 }
